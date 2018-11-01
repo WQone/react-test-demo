4e8303ea09b03f1c239f7754bd4abae9798a7ba3
wuqian <1307144731@qq.com>
2018-11-01 16:52:14 +0800

    React + Dva + Antd + umi+sass

---
+++ .umirc.js
@@ -0,0 +1,41 @@
+// ref: https://umijs.org/config/
+export default {
+  plugins: [
+    // ref: https://umijs.org/plugin/umi-plugin-react.html
+    [
+      'umi-plugin-react',
+      {
+        antd: true,
+        dva: true,
+        dynamicImport: false,
+        title: 'react-test-demo',
+        dll: false,
+        routes: {
+          exclude: [],
+        },
+        hardSource: false,
+      },
+    ],
+  ],
+  routes: [
+    {
+      path: '/',
+      component: '../layouts',
+      routes: [
+        {
+          path: '',
+          component: './Hello/Hello',
+        },
+      ],
+    },
+  ],
+  copy: [
+    {
+      from: './public',
+      to: './public',
+      ignore: ['.*'],
+    },
+  ],
+  sass: {},
+  disableCSSModules: true,
+};
